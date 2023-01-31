bda6ead71e5902a32525aa2de894f18316d07292
swordgeo <swordgeo1094@yahoo.com>
2023-01-31 13:02:28 -0500

    rename fields from "cse341-l8" to "boardgamegeeks"

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -2,7 +2,7 @@ const checkLogin = (req, res, next) => {
   if (!req.oidc.isAuthenticated()) {
     return res.status(401).send({
       error: 'Not authorized to change data without login.',
-      login: "https://cse341-l8-onrender.com/login"
+      login: "https://boardgamegeeks-onrender.com/login"
     });
   }
   next();
