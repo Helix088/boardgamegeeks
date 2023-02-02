e73de8628fdb5eb44619567f98d6f6441d3272b4
swordgeo <swordgeo1094@yahoo.com>
2023-02-02 16:28:44 -0500

    Users working on Swagger

--- middleware/userlink.js
+++ middleware/userlink.js
@@ -1,59 +1,10 @@
-const security = require('./authorize.js');
 const usersController = require('../controllers/users');
 
-// The idea of this middleware is to, first, perform the checkLogin action (to see if we're logged in)
-//If so, we're then going to check their oidc info versus our databse and try to find the corresponding user
-//If we cannot find them (i.e. don't exist yet), we perform a post to create the user, before moving forward
+// The idea of this middleware is to, first, 
+//check their oidc info versus our databse and try to find the corresponding user
+//If we cannot find them (i.e. don't exist yet), 
+//we perform a post to create the user, before moving forward
 //finally we keep it moving with next()
-
-
-// For some reason doing it like this won't properly catch a logged out user.
-//So I'm forced to add it back into routes before this call
-// security.checkLogin;
-
-// const findUser = (req, res, next) => {
-//   // res.send(JSON.stringify(req.oidc.user));
-//   console.log("userlink.js line 13 ", (req.oidc.user.email));
-
-//   try {
-//     usertest = usersController.getUserByUserlink(req, res, next);
-//     console.log("Outside getUserbyUserlink userlink.js line 17");
-
-//     if (usertest) {
-//       res.status(200).json(usertest);
-//     } else {
-//       console.log("Couldn't find them...creating...")
-//       newuser = usersController.addUserbyUserlink(req, res, next);
-//       res.status(200).json(newuser);
-//     }
-    
-    
-//   } catch (err) {
-//     // We assume this happens when user not found as they don't exist. Sadly there could be other reasons; might want to rethink this idea
-//     console.log("userlink.js catchblock");
-//     console.log("Couldn't find them...creating...")
-//     usersController.addUserbyUserlink(req, res, next);
-//   };
-
-//   next();
-// }
-
-
-// const findUser = async (req, res, next) => {
-//   console.log("userlink.js line 13 ", (req.oidc.user.email));
-
-//   const usertest = await usersController.getUserByUserlink(req, res, next);
-
-//   console.log("Outside getUserbyUserlink userlink.js line 17");
-//   if (!usertest) {
-//     console.log("Couldn't find them...creating...")
-//     await usersController.addUserByUserlink(req, res, next);
-//   } else {
-//     res.json(usertest);
-//   }
-  
-//   next();
-// };
 
 
 const findUser = async (req, res, next) => {
@@ -72,16 +23,16 @@ const findUser = async (req, res, next) => {
     let createduser = await usersController.addUserByUserlink(req, res, next);
     req.user = createduser;
   };
-
-  
-
   next();
 }
 
-const printUsername = async (req, res, next) => {
-  console.log("Username: " + req.user.username);
-  next();
+//just for testing
+// const printUsername = async (req, res, next) => {
+//   console.log("Username: " + req.user.username);
+//   next();
 
-}
+// }
 
-module.exports = {findUser, printUsername};
+// module.exports = {findUser, printUsername};
+
+module.exports = {findUser};
--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,16 @@
 const express = require('express');
 const routes = express.Router();
-const userlink = require('../middleware/userlink.js');
+// const userlink = require('../middleware/userlink.js');
 const security = require('../middleware/authorize.js');
 const usersController = require('../controllers/users');
 
 //userlink experiment
 // routes.get('/', security.checkLogin, userlink.findUser); 
 
-
-//OAuth
-
-// routes.get('/:id', usersController.getUser);
-// routes.get('/:email', usersController.getUserByEmail);
-// routes.post('/', security.checkLogin, usersController.addUser);
-// routes.patch('/:id', security.checkLogin, usersController.editUser);
-// routes.delete('/:id', security.checkLogin, usersController.delUser);
-
-
-//Weenie hut junior with no security
-
 routes.get('/', usersController.getUsers);
 routes.get('/:id', usersController.getUser);
-// routes.get('/:email', usersController.getUserByEmail);
-routes.post('/', security.checkLogin, userlink.findUser, userlink.printUsername, usersController.addUser);
-routes.patch('/:id', usersController.editUser);
-routes.delete('/:id', usersController.delUser);
+routes.post('/', security.checkLogin, usersController.addUser);
+routes.patch('/:id', security.checkLogin, usersController.editUser);
+routes.delete('/:id', security.checkLogin, usersController.delUser);
 
 module.exports = routes; 
